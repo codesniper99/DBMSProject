7eda0b51338ab8d2e0ff42688907b06772fcd7ec
Akhil <akhilvaid21@gmail.com>
2018-10-18 15:37:40 +0530

    "trying ek aur

---
+++ src/JavaApplication2.java
@@ -0,0 +1,27 @@
+import java.io.PrintStream;
+import java.util.*;
+import javaapplication2.Login_As;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author HAPPY
+ */
+public class JavaApplication2 extends Login_As {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    Login_As obj= new Login_As();
+    obj.setVisible(true);
+        
+        
+    }
+    
+}
